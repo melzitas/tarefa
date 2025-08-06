1147b3881c925f4050a44e0fa749f8a9fd67fd1a
mel <eufraziomelissa@gmail.com>
2025-08-06 10:48:28 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,27 @@
+const CACHE_NAME = 'pwa-task-list-v1';
+const urlsToCache = [
+    '/tarefa/sw',
+    '/tarefa/index.html',
+    '/tarefa/style.css',
+    '/tarefa/manifest.json',
+    '/tarefa/icons/icon-192x192.png',
+    '/tarefa/icons/icon-512x512.png'
+];
+
+self.addEventListener('install', event => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then(cache => {
+                return cache.addAll(urlsToCache);
+            })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    event.respondWith(
+        caches.match(event.request)
+            .then(response => {
+                return response || fetch(event.request);
+            })
+    );
+});
